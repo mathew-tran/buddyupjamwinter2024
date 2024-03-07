3c285ad43b81b407f45bb8ac3ff6640ac17f064d
Mathew <mathew.tran.dev@gmail.com>
2024-03-07 12:49:02 -0800

    add UI to show how much is in storage

--- roborenewal/Assets/Scripts/Player/CogHolder.cs
+++ roborenewal/Assets/Scripts/Player/CogHolder.cs
@@ -1,21 +1,33 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CogHolder : MonoBehaviour
 {
-    public int Limit = 100;
-    public int CurrentHolding = 0;
+    public int mLimit = 100;
+    public int mCurrentHolding = 0;
 
+    public Action<int> OnUpdateCogHolder;
     public void TakeCog(GameObject cog)
     {
-        CurrentHolding += 1;
+        mCurrentHolding += 1;
         cog.gameObject.GetComponent<BoxCollider>().enabled = false;
         Destroy(cog);
+        OnUpdateCogHolder(mCurrentHolding);
     }
 
     public bool IsFull()
     {
-        return CurrentHolding >= Limit;
+        return mCurrentHolding >= mLimit;
+    }
+
+    public int GetLimit()
+    {
+        return mLimit;
+    }
+
+    public int GetCurrentAmount() {
+        return mCurrentHolding;
     }
 }
