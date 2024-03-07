2dfc053a022ee535021a0bf2630dbda8a30fda7d
Mathew <mathew.tran.dev@gmail.com>
2024-03-06 20:43:39 -0800

    animate garbage when hit

--- roborenewal/Assets/Scripts/Garbage.cs
+++ roborenewal/Assets/Scripts/Garbage.cs
@@ -7,18 +7,22 @@ public class Garbage : MonoBehaviour
 {
     public Timer mTimer;
     public HealthComponent mHealthComponent;
-
+    public Animator mAnimator;
     private void Start()
     {
         mHealthComponent.OnTakeDamage += OnGarbageTakeDamage;
         mHealthComponent.OnDeath += OnGarbageDeath;
         mTimer.OnTimeout += OnTimerTimeout;
+        OnTimerTimeout();
     }
+
 
     private void OnGarbageTakeDamage()
     {
         mHealthComponent.mbCanTakeDamage = false;
         mTimer.StartTimer();
+        mAnimator.Play("ANIM_GarbageHit");
+        mAnimator.speed = 3;
     }
 
     private void OnGarbageDeath()
@@ -29,6 +33,9 @@ public class Garbage : MonoBehaviour
     private void OnTimerTimeout()
     {
         mHealthComponent.mbCanTakeDamage = true;
+        mAnimator.speed = 0;
+        mAnimator.Play("ANIM_GarbageHit", 0, 0);
+        mAnimator.Update(0);
     }
     private void OnTriggerStay(Collider other)
     {
